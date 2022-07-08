92dc26ee4f03ffbcd6b30644acd0701b35c5883b
Gregor499 <postfach@gregor-dirks.de>
2022-07-08 16:41:20 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
-function App() {
-
-    const [greeting, setGreeting] = useState('')
-
-    useEffect(() => {
-        fetch('/api/greeting', {
-            method: 'GET',
-            headers: {
-                'Accept': 'text/plain'
-            }
-        })
-            .then(response => response.text())
-            .then(text => setGreeting(text))
-            .catch(err => setGreeting('Da ist etwas schief gelaufen'));
-    }, []);
+export default function App() {
 
     return (
         <div>
-            {greeting}
+            test
         </div>
     );
 }
-
-export default App;
