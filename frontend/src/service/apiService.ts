e9b4bec3a071038d38ae260344a61ebadddf32d0
Gregor499 <postfach@gregor-dirks.de>
2022-07-24 18:46:50 +0200

    code formatted

--- frontend/src/service/apiService.ts
+++ frontend/src/service/apiService.ts
@@ -2,11 +2,11 @@ import {LoginData, LoginResponse, TimeUnit, UserCreationData} from "./models";
 import axios, {AxiosResponse} from "axios";
 import TimeUnits from "../timetable/TimeUnits";
 
-export const registerUser = (userCreationData: UserCreationData) =>{
+export const registerUser = (userCreationData: UserCreationData) => {
     return axios.post("api/users", userCreationData)
 }
 
-export const loginUser = (loginData: LoginData) =>{
+export const loginUser = (loginData: LoginData) => {
     return axios.post("api/auth/login", loginData)
         .then((response: AxiosResponse<LoginResponse>) => response.data)
 }
