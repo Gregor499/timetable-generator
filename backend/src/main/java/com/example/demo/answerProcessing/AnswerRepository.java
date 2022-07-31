4df954d0aa5c5b8959f9d58c34953f015b24a4d0
Gregor499 <postfach@gregor-dirks.de>
2022-07-31 21:29:27 +0200

    Each new TimeAnswer is now potentially replacing an older one which has the same UserId and QuestionId

--- backend/src/main/java/com/example/demo/answerProcessing/AnswerRepository.java
+++ backend/src/main/java/com/example/demo/answerProcessing/AnswerRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.answerProcessing;
 
-import com.example.demo.timetable.TimeUnit;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface AnswerRepository extends MongoRepository<TimeAnswer, String> {
+    Optional<TimeAnswer> findByUserIdAndQuestionId(String userId, String questionId);
 }
--- backend/src/main/java/com/example/demo/answerProcessing/AnswerService.java
+++ backend/src/main/java/com/example/demo/answerProcessing/AnswerService.java
@@ -1,9 +1,18 @@
 package com.example.demo.answerProcessing;
 
+import com.example.demo.timetable.TimeUnit;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.MongoCollection;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
+import javax.management.Query;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,11 +20,17 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
     public void addNewAnswer(TimeAnswer timeAnswer) {
-
+        if (findByUserIdAndQuestionId(timeAnswer).isPresent()) {
+            answerRepository.delete(findByUserIdAndQuestionId(timeAnswer).orElseThrow());
+        }
         answerRepository.save(timeAnswer);
     }
 
     public List<TimeAnswer> findAll() {
         return answerRepository.findAll();
+    }
+
+    public Optional<TimeAnswer> findByUserIdAndQuestionId(TimeAnswer timeAnswer) {
+        return answerRepository.findByUserIdAndQuestionId(timeAnswer.getUserId(), timeAnswer.getQuestionId());
     }
 }
