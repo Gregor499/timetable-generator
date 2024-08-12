9d8dbcc8139b708832f48db28671f032361a225e
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2024-08-12 20:51:25 +0200

    - fixed black-screen error - using async/await syntax for better readability - replaced different workday hooks in one combined array - added error logging - separated time to minute converter - replaced separate rendering for each workday with a generic approach
    - learned: "...exampleArray" in e.g. [...arrayName, 4, 5] with exampleArray = [1,2,3] results in: [1,2,3,4,5]

--- frontend/src/questions/QuestionListComponent.tsx
+++ frontend/src/questions/QuestionListComponent.tsx
@@ -13,17 +13,24 @@ interface QuestionProps {
 export default function QuestionListComponent(props: QuestionProps) {
     const [timeUnitList, setTimeUnitList] = useState<Array<TimeUnit>>([])
     const [currentTimeAnswer, setCurrentTimeAnswer] = useState<string>()
-
-    const [monday, setMonday] = useState<boolean>(false)
-    const [tuesday, setTuesday] = useState<boolean>(false)
-    const [wednesday, setWednesday] = useState<boolean>(false)
-    const [thursday, setThursday] = useState<boolean>(false)
-    const [friday, setFriday] = useState<boolean>(false)
-    const [saturday, setSaturday] = useState<boolean>(false)
-    const [sunday, setSunday] = useState<boolean>(false)
-
-
+    const [workdays, setWorkdays] = useState<boolean[]>(Array(7).fill(false));
     const [errorMessage, setErrorMessage] = useState("")
+
+    useEffect(() => {
+        const loadTimeUnitList = async () => {
+            try {
+                const data = await getTimeUnitList();
+                setTimeUnitList(data);
+                } catch {
+                    setErrorMessage("Failed to load time unit list")
+                }
+            };
+
+            loadTimeUnitList();
+
+        const currentAnswer = props.answers.find(answer => answer.questionId === props.question.id)
+        setCurrentTimeAnswer(currentAnswer ? currentAnswer.time : "XX:XX");
+    }, [props.answers, props.question.id])
 
     const setTimeAnswer = (timeInMinutes: number) => {
         const timeAnswer: TimeAnswer = {
@@ -33,40 +40,39 @@ export default function QuestionListComponent(props: QuestionProps) {
         }
         postTimeAnswer(timeAnswer)
             .then(() => props.answerCallback())
-            .catch(() => setErrorMessage("error posting answer"))
-    }
+            .catch(() => {
+                console.error("Error posting time answer:", Error);
+                setErrorMessage("error posting answer");
+                })
+    };
 
-    const setWorkdayAnswer = () => {
-        const workdayAnswer: WorkdayAnswer = {
-            questionId: props.question.id,
-            question: props.question.question,
-            monday: monday,
-            tuesday: tuesday,
-            wednesday: wednesday,
-            thursday: thursday,
-            friday: friday,
-            saturday: saturday,
-            sunday: sunday
 
-        }
+    const handleSubmit = () => {
+                const workdayAnswer: WorkdayAnswer = {
+                    questionId: props.question.id,
+                    question: props.question.question,
+                    monday: workdays[0],
+                    tuesday: workdays[1],
+                    wednesday: workdays[2],
+                    thursday: workdays[3],
+                    friday: workdays[4],
+                    saturday: workdays[5],
+                    sunday: workdays[6],
+                };
+
         postWorkdayAnswer(workdayAnswer)
             .then(() => props.answerCallback())
-            .catch(() => setErrorMessage("error posting answer"))
-    }
+            .catch(() => {
+                console.error("Error posting workday answer:", Error);
+                setErrorMessage("error posting answer");
+                })
+    };
 
-    useEffect(() => {
-        getTimeUnitList()
-            .then(data => setTimeUnitList(data))
-            .catch(() => setErrorMessage("timeUnitList does not load"));
-
-        const currentAnswer = props.answers.find(answer => answer.questionId === props.question.id)
-        if (currentAnswer) {
-            setCurrentTimeAnswer(currentAnswer.time!)
-        } else {
-            setCurrentTimeAnswer("XX:XX")
-        }
-
-    }, [props.answers, props.question.id])
+    const convertTimeToMinutes = (time: string | undefined): number => {
+        if(!time) return 0;
+        const parts = time.split(":");
+        return (Number(parts[0]) * 60) + Number(parts[1]);
+        };
 
     const timeUnitsToChoose = timeUnitList
         .filter(timeUnit => {
@@ -75,9 +81,10 @@ export default function QuestionListComponent(props: QuestionProps) {
             } else {
                 const previousQuestionAnswer = props.answers.find(answer => answer.questionId === props.question.previousQuestionId)
                 if (previousQuestionAnswer) {
-                    const currentTimeAnswerInMinutes = (Number(currentTimeAnswer![0].charAt(0)) * 600 + Number(currentTimeAnswer![0].charAt(1)) * 60 + Number(currentTimeAnswer![0].charAt(3)) * 10
-                        + Number(currentTimeAnswer![0].charAt(5)));
-                    return (timeUnit.timeInMinutes! >= previousQuestionAnswer.timeInMinutes) && timeUnit.timeInMinutes !== currentTimeAnswerInMinutes;
+                    const currentTimeAnswerInMinutes = convertTimeToMinutes(currentTimeAnswer);
+                    return (
+                        timeUnit.timeInMinutes! >= previousQuestionAnswer.timeInMinutes) &&
+                        timeUnit.timeInMinutes !== currentTimeAnswerInMinutes;
                 } else {
                     return true;
                 }
@@ -85,60 +92,58 @@ export default function QuestionListComponent(props: QuestionProps) {
         })
         .map(timeUnit => <TimeAnswerProperties key={timeUnit.id} timeUnit={timeUnit}/>)
 
-    const QuestionType = () => {
-        if (props.question.question === "On which days do you work ?"){
-                    return (
-                    <div>
-                    	<span>
-                            <input type="checkbox" name="workday" value="monday" id="check1" onChange={event => {setMonday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check1">monday</label>
-                        </span>
-                    	<span>
-                    	    <input type="checkbox" name="workday" value="tuesday" id="check2" onChange={event => {setTuesday(event.target.checked); setWorkdayAnswer()}}/>
-                    	    <label htmlFor="check2">tuesday</label>
-                    	</span>
-                    	<span>
-                            <input type="checkbox" name="workday" value="wednesday" id="check3" onChange={event => {setWednesday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check3">wednesday</label>
-                    	</span>
-                    	<span>
-                            <input type="checkbox" name="workday" value="thursday" id="check4" onChange={event => {setThursday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check4">thursday</label>
-                        </span>
-                        <span>
-                            <input type="checkbox" name="workday" value="friday" id="check5" onChange={event => {setFriday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check5">friday</label>
-                        </span>
-                        <span>
-                            <input type="checkbox" name="workday" value="saturday" id="check6" onChange={event => {setSaturday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check6">saturday</label>
-                        </span>
-                        <span>
-                            <input type="checkbox" name="workday" value="sunday" id="check7" onChange={event => {setSunday(event.target.checked); setWorkdayAnswer()}}/>
-                            <label htmlFor="check7">sunday</label>
-                        </span>
-                    </div>)
-//bug: last checked checkbox isnt being recognized
+       const renderWorkdayCheckboxes = () => {
+           const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+           return days.map((day, index) => (
+               <span key={index}>
+                   <input
+                       type="checkbox"
+                       id={`check${index}`}
+                       checked={workdays[index]}
+                       onChange={event => {
+                           const newWorkdays = [...workdays];
+                           newWorkdays[index] = event.target.checked;
+                           setWorkdays(newWorkdays);
+                       }}
+                   />
+                   <label htmlFor={`check${index}`}>{day}</label>
+               </span>
+           ));
+       };
 
-        }
+       const QuestionType = () => {
+           if (props.question.question === "On which days do you work ?") {
+               return (
+                   <div>
+                       {renderWorkdayCheckboxes()}
+                       <button className="submit" onClick={handleSubmit}>Submit</button>
+                   </div>
+               );
+           }
 
-       else{
-       return(
-       <div>
-           <select className="questionAnswer" name={props.question.type} id={props.question.id}
-                               onChange={event => setTimeAnswer(Number(event.target.value))}>
-
-                                  value=<TimeAnswerProperties key={currentTimeAnswer} timeUnit={{
-                                  time: currentTimeAnswer + "",
-                                  length: 15,
-                                  end: "24:00"
-                              }}/>
-
+           else{
+               return(
+               <div>
+                   <select
+                       className="questionAnswer"
+                       name={props.question.type}
+                       id={props.question.id}
+                       onChange={event => setTimeAnswer(Number(event.target.value))}
+                   >
+                              value=<TimeAnswerProperties
+                              key={currentTimeAnswer}
+                              timeUnit={{
+                                time: currentTimeAnswer + "",
+                                length: 15,
+                                 end: "24:00"
+                              }}
+                              />
                               {timeUnitsToChoose}
-           </select>
-       </div>)
-       }
-    }
+                   </select>
+               </div>
+               );
+           }
+        };
 
     return (
         <div className="question">
@@ -146,5 +151,5 @@ export default function QuestionListComponent(props: QuestionProps) {
             {errorMessage && <div>{errorMessage}</div>}
             {QuestionType()}
         </div>
-    )
+    );
 }
