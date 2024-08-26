cf9f3201cb60d514dbd1434456e75ae6f8cab83f
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2024-08-26 16:10:32 +0200

    - generified process() in TimeAnswerProcessingService by outsourcing repeating methods into extra method - Bug: TimeUnit of Sleep are calculated correctly, but in Frontend the fields before sleep are now being displayed again and not removed like before

--- backend/src/main/java/application/answerProcessing/TimeAnswerProcessingService.java
+++ backend/src/main/java/application/answerProcessing/TimeAnswerProcessingService.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TimeAnswerProcessingService {
     private static final String WORKDAY_QUESTION = "On which days do you work ?";
+    private static final int OFFSET = 60;
 
     private final TimeAnswerService timeAnswerService;
     private final WeekdayAnswerService weekdayAnswerService;
@@ -52,33 +53,44 @@ public class TimeAnswerProcessingService {
     }
 
     public void process(String userId) throws Exception {
-        safeProcessedAnswer("morningSleep", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                setRenderCap(timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to wake up ?").orElseThrow().getTimeInMinutes(), - 60),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to wake up ?").orElseThrow().getTime());
+        genericProcessing(userId, "morningSleep", WORKDAY_QUESTION,
+                "When do you want to wake up ?", "When do you want to wake up ?", -OFFSET);
 
-        safeProcessedAnswer("morningRoutine", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to wake up ?").orElseThrow().getTime(),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When are you ready for the day?").orElseThrow().getTime());
+        //morningRoutine is connected to morningSleep
+        genericProcessing(userId, "morningRoutine", WORKDAY_QUESTION,
+                "When do you want to wake up ?", "When are you ready for the day?", 0);
 
-        safeProcessedAnswer("workWayTime", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to begin going to work ?").orElseThrow().getTime(),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When does your work start ?").orElseThrow().getTime());
+        genericProcessing(userId, "workWayTime", WORKDAY_QUESTION,
+                "When do you want to begin going to work ?", "When does your work start ?", 0);
 
-        safeProcessedAnswer("work", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When does your work start ?").orElseThrow().getTime(),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When does your work end ?").orElseThrow().getTime());
+        genericProcessing(userId, "work", WORKDAY_QUESTION,
+                "When does your work start ?", "When does your work end ?", 0);
 
-        safeProcessedAnswer("leisureTime", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When does your leisure time start ?").orElseThrow().getTime(),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When does your leisure time end ?").orElseThrow().getTime());
+        genericProcessing(userId, "leisureTime", WORKDAY_QUESTION,
+                "When does your leisure time start ?", "When does your leisure time end ?", 0);
 
-        safeProcessedAnswer("eveningRoutine", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to start to get ready for bed ?").orElseThrow().getTime(),
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to sleep ?").orElseThrow().getTime());
+        //eveningRoutine is connected to nightSleep
+        genericProcessing(userId, "eveningRoutine", WORKDAY_QUESTION,
+                "When do you want to start to get ready for bed ?", "When do you want to sleep ?", 0);
 
-        safeProcessedAnswer("nightSleep", getWeekdays(userId, WORKDAY_QUESTION), userId,
-                timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to sleep ?").orElseThrow().getTime(),
-                setRenderCap(timeAnswerService.findByUserIdAndQuestion(userId, "When do you want to sleep ?").orElseThrow().getTimeInMinutes(), + 60));
+        genericProcessing(userId, "nightSleep", WORKDAY_QUESTION, "When do you want to sleep ?", "When do you want to sleep ?", OFFSET);
+    }
+
+    public void genericProcessing(String userId, String questionType, String weekdayType, String startTimeQuestion, String endTimeQuestion, int shift) throws Exception {
+        if (shift < 0) {
+            safeProcessedAnswer(questionType, getWeekdays(userId, weekdayType), userId,
+                    setRenderCap(timeAnswerService.findByUserIdAndQuestion(userId, startTimeQuestion).orElseThrow().getTimeInMinutes(), shift),
+                    timeAnswerService.findByUserIdAndQuestion(userId, endTimeQuestion).orElseThrow().getTime());
+        }
+        if (shift > 0) {
+            safeProcessedAnswer(questionType, getWeekdays(userId, weekdayType), userId,
+                    timeAnswerService.findByUserIdAndQuestion(userId, startTimeQuestion).orElseThrow().getTime(),
+                    setRenderCap(timeAnswerService.findByUserIdAndQuestion(userId, endTimeQuestion).orElseThrow().getTimeInMinutes(), shift));
+        } else {
+            safeProcessedAnswer(questionType, getWeekdays(userId, weekdayType), userId,
+                    timeAnswerService.findByUserIdAndQuestion(userId, startTimeQuestion).orElseThrow().getTime(),
+                    timeAnswerService.findByUserIdAndQuestion(userId, endTimeQuestion).orElseThrow().getTime());
+        }
     }
 
     public String setRenderCap(int timeInMinutes, int shift) {
@@ -114,13 +126,13 @@ public class TimeAnswerProcessingService {
                 .orElseThrow(() -> new Exception("Error: weekdayAnswer not available!"));
 
         Map<String, Boolean> weekdays = new HashMap<>();
-            weekdays.put("monday", weekdayAnswer.isMonday());
-            weekdays.put("tuesday", weekdayAnswer.isTuesday());
-            weekdays.put("wednesday", weekdayAnswer.isWednesday());
-            weekdays.put("thursday", weekdayAnswer.isThursday());
-            weekdays.put("friday", weekdayAnswer.isFriday());
-            weekdays.put("saturday", weekdayAnswer.isSaturday());
-            weekdays.put("sunday", weekdayAnswer.isSunday());
+        weekdays.put("monday", weekdayAnswer.isMonday());
+        weekdays.put("tuesday", weekdayAnswer.isTuesday());
+        weekdays.put("wednesday", weekdayAnswer.isWednesday());
+        weekdays.put("thursday", weekdayAnswer.isThursday());
+        weekdays.put("friday", weekdayAnswer.isFriday());
+        weekdays.put("saturday", weekdayAnswer.isSaturday());
+        weekdays.put("sunday", weekdayAnswer.isSunday());
 
         return weekdays;
     }
