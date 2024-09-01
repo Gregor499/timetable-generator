cf166566c79bf06341e4d102869f74fccc514910
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2024-09-01 15:46:05 +0200

    - refactored code

--- frontend/src/timetable/Timetable.tsx
+++ frontend/src/timetable/Timetable.tsx
@@ -4,15 +4,14 @@ import { getProcessedTimeAnswers, getTimeUnitList } from "../service/apiService"
 import { ProcessedTimeAnswer, TimeUnit } from "../service/models";
 import TimeTableContent from "./TimeTableContent";
 import { convertTimeUnitToMinutes } from "../utilities/Util"
-//@ts-ignore
-import { useScreenshot, createFileName } from "use-react-screenshot";
+//import { useScreenshot, createFileName } from "use-react-screenshot";
 
 export default function Timetable() {
     const [timeUnitList, setTimeUnitList] = useState<Array<TimeUnit>>([])
     const [processedTimeAnswerList, setProcessedTimeAnswerList] = useState<Array<ProcessedTimeAnswer>>([])
     const [errorMessage, setErrorMessage] = useState("")
 
-    const ref = createRef();
+/*     const ref = createRef();
       const [image, takeScreenShot] = useScreenshot({
         type: "image/jpeg",
         quality: 1.0
@@ -25,16 +24,16 @@ export default function Timetable() {
         a.click();
       };
 
-      const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+      const downloadScreenshot = () => takeScreenShot(ref.current).then(download); */
 
     useEffect(() => {
         getTimeUnitList()
             .then(data => setTimeUnitList(data))
-            .catch(() => setErrorMessage("timeUnitList does not load"));
+            .catch(() => setErrorMessage("Error in loading TimeUnit list :\nCheck if the backend is running and reload page !"));
 
         getProcessedTimeAnswers()
             .then(data => setProcessedTimeAnswerList(data))
-            .catch(() => setErrorMessage("processedAnswerList does not load"));
+            .catch(() => setErrorMessage("Error while processing Answers:\nCheck if one is missing or falsely set !"));
     }, []);
 
     let maxStart = 0
@@ -49,85 +48,79 @@ export default function Timetable() {
         }
     })
 
+
+/*
+separate to new generateTimeTableContent function
+ */
     const timeTableContent = timeUnitList
         .filter(timeUnit => timeUnit.timeInMinutes! >= maxStart && timeUnit.timeInMinutes! <= maxEnd)
         .map(timeUnit => {
-            let task = ""
-            let monday: boolean = false
-            let tuesday: boolean = false
-            let wednesday: boolean = false
-            let thursday: boolean = false
-            let friday: boolean = false
-            let saturday: boolean = false
-            let sunday: boolean = false
+            const matchingAnswer = findMatchingAnswer(timeUnit.time, processedTimeAnswerList);
 
-            processedTimeAnswerList.forEach(processedTimeAnswer => {
-                processedTimeAnswer.timeList.forEach(time => {
-                    if (time.includes(timeUnit.time)) {
-                        task = (processedTimeAnswer.task)
-                        monday = (processedTimeAnswer.monday)
-                        tuesday = (processedTimeAnswer.tuesday)
-                        wednesday = (processedTimeAnswer.wednesday)
-                        thursday = (processedTimeAnswer.thursday)
-                        friday = (processedTimeAnswer.friday)
-                        saturday = (processedTimeAnswer.saturday)
-                        sunday = (processedTimeAnswer.sunday)
-                    }
-                })
-            })
             return <TimeTableContent
                         key={timeUnit.id}
                         timeUnit={timeUnit}
-                        task={task}
-                        monday={monday}
-                        tuesday={tuesday}
-                        wednesday={wednesday}
-                        thursday={thursday}
-                        friday={friday}
-                        saturday = {saturday}
-                        sunday = {sunday}
+                        task={matchingAnswer?.task || ""}
+                        monday={matchingAnswer?.monday || false}
+                        tuesday={matchingAnswer?.tuesday || false}
+                        wednesday={matchingAnswer?.wednesday || false}
+                        thursday={matchingAnswer?.thursday || false}
+                        friday={matchingAnswer?.friday || false}
+                        saturday = {matchingAnswer?.saturday || false}
+                        sunday = {matchingAnswer?.sunday || false}
                    />
         })
+/*
+separate to new generateTimeTableContent function
+ */
 
+
+
+//onClick={downloadScreenshot}
     return (
         <>
             <h1 className="headline">Timetable</h1>
             <div>
-              <button onClick={downloadScreenshot}>Download screenshot</button>
+              <button >Download screenshot</button>
               <div className="flex-container">
-                              <table className="table">
-                                  <thead>
-                                  <tr className="flex-item">
-                                      <th className="timeUnits">Time</th>
-                                      <th>Monday</th>
-                                      <th>Tuesday</th>
-                                      <th>Wednesday</th>
-                                      <th>Thursday</th>
-                                      <th>Friday</th>
-                                      <th>Saturday</th>
-                                      <th>Sunday</th>
-                                  </tr>
-                                  </thead>
+                  <table className="table">
+                      <thead>
+                      <tr className="flex-item">
+                          <th className="timeUnits">Time</th>
+                          <th>Monday</th>
+                          <th>Tuesday</th>
+                          <th>Wednesday</th>
+                          <th>Thursday</th>
+                          <th>Friday</th>
+                          <th>Saturday</th>
+                          <th>Sunday</th>
+                      </tr>
+                      </thead>
 
-                                  <tbody>
-                                  {timeTableContent}
-
-                                  {errorMessage && <tr>
-                                      <th>{errorMessage}</th>
-                                      <th></th>
-                                      <th></th>
-                                      <th></th>
-                                      <th></th>
-                                      <th></th>
-                                      <th></th>
-                                      <th></th>
-                                  </tr>}
-                                  </tbody>
-                              </table>
-                          </div>
+                      <tbody>
+                      {timeTableContent}
+                      {errorMessage && (
+                        <tr>
+                            <th colSpan={8}>{errorMessage}</th>
+                        </tr>
+                        )}
+                      </tbody>
+                  </table>
+              </div>
             </div>
 
         </>
 
     );
+}
+
+// Find the processed answer that matches the current time unit
+function findMatchingAnswer(time: string, processedTimeAnswerList: ProcessedTimeAnswer[]) {
+    for (const processedTimeAnswer of processedTimeAnswerList) {
+        if (processedTimeAnswer.timeList.includes(time)){
+            return processedTimeAnswer;
+           }
+    }
+    return null;
+
 }
