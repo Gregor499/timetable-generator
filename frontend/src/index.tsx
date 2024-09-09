7cd9a8b4c92b230373aeacd07bbf2e0b34772a3d
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2024-09-09 17:58:26 +0200

    - fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CssBaseline, Box, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
