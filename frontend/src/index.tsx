afb779b52440767943ff138a8807abeaa2c26611
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2024-10-22 23:38:37 +0200

    - expanded theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,17 +8,28 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { blue, green, purple } from '@mui/material/colors';
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 const theme = createTheme({
     palette: {
+/*       primary: {
+        main: blue[500],
+      }, */
+      secondary: {
+        main: blue[400],
+      },
       background: {
         default: '#DAA520', // Set your universal background color here
       },
       text: {
-        primary: '#000000', // Set the default text color if needed
+        primary: '#FFFFFF', // Set the default text color if needed
+      },
+      action: {
+        selected: '#1976D2', // Set the color for selected items
+        hover: 'blue[400]', // Set the color for hovered items
       },
     },
   });
